176b129e820a5a36923cbb99c9f6324ecadc837c
abaa3023 <abaa3023@colorado.edu>
2022-10-16 14:48:37 -0600

    Push updates, working in local

--- base_external/rootfs_overlay/etc/init.d/S98lddmodules.sh
+++ base_external/rootfs_overlay/etc/init.d/S98lddmodules.sh
@@ -1,13 +1,14 @@
-
 #! /bin/sh
 
 case "$1" in
     start)
+        echo "Loading module misc-modules and scull"
         modprobe hello
         /usr/bin/module_load faulty
         /usr/bin/scull_load
         ;;
     stop)
+        echo "Stopping scull and misc-modules"
         rmmod hello
         /usr/bin/module_unload faulty
         /usr/bin/scull_unload
