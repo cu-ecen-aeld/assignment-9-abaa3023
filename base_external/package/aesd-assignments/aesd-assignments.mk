29434155063f2ba1bef16c5f23e228927014a865
abaa3023 <abaa3023@colorado.edu>
2022-10-16 02:56:48 -0600

    Push

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = f5fc8f8fac71ddf1969b2040adfca67c34f54ab6
+AESD_ASSIGNMENTS_VERSION = b4247d9e1f295398663cd7ea5d7c33ec90853314
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
